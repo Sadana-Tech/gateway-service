5d6629b16c7a2796cca9baabbce63108fe6b4075
Sadana <101989041+Sadana-Tech@users.noreply.github.com>
2024-09-30 05:50:44 +0530

    COnfigure product service
    COnfigure product service

--- src/main/java/com/sadana/tech/filter/CustomGlobalFilter.java
+++ src/main/java/com/sadana/tech/filter/CustomGlobalFilter.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Configuration
+//@Configuration
 @Slf4j
 public class CustomGlobalFilter {
 
